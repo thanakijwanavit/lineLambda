51dc3d081de5b7da8bbe907906a735516f651ddf
nic <nwanavit@hatari.cc>
2020-10-12 17:27:03 +0700

    add line token

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -1,2 +1,2 @@
 sam build --profile villaaws &&\
-  sam deploy --profile villaaws --parameter-overrides LINEACCESSKEY=$(cat ~/.line)
+sam deploy --profile villaaws --parameter-overrides LINEACCESSKEY=$(cat ~/.line)
